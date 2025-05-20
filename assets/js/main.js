2b461aef4bfb8959790dd42433cc0ced719cd557
Mukesh Kushwaha <205293204+mukeshkushwahae@users.noreply.github.com>
2025-05-20 13:52:03 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+// Query Selectors
+const doc = document;
+const getId = "getElementById";
+const rawCode = doc[getId]("rawCode");
+const parsedCode = doc[getId]("parsedCode");
+const parseBtn = doc[getId]("parseBtn");
+const copyBtn = doc[getId]("copyBtn");
+const clearBtn = doc[getId]("clearBtn");
+const parsedCodeHidden = doc[getId]("parsedCodeHidden");
+
+// Parse Button
+parseBtn.addEventListener("click", function () {
+   if (rawCode.value === "") {
+      window.alert("Oops, you haven't added any code.");
+      return false;
+   } else {
+      parseBtn.innerHTML = "Parsing...";
+      parseBtn.disabled = true;
+      setTimeout(() => {
+         parsedCode.value = rawCode.value
+            .replace(/&/ig, "&amp;")
+            .replace(/</ig, "&lt;")
+            .replace(/>/ig, "&gt;")
+            .replace(/"/ig, "&quot;")
+            .replace(/'/ig, "&#039;")
+            .replace(/&#177;/ig, "&plusmn;")
+            .replace(/&#169;/ig, "&copy;")
+            .replace(/&#174;/ig, "&reg;")
+            .replace(/ya'll/ig, "ya'll");
+         parseBtn.innerHTML = "Parse";
+         parseBtn.disabled = false;
+         parsedCodeHidden.classList.remove("hidden");
+      }, 1000);
+   }
+});
+
+// Copy Button
+copyBtn.addEventListener("click", function () {
+   if (rawCode.value === "") {
+      window.alert("Oops, you haven't added any code.");
+      return false;
+   } else {
+      copyBtn.innerHTML = "Copying...";
+      copyBtn.disabled = true;
+      setTimeout(() => {
+         copyBtn.innerHTML = "Copy";
+         copyBtn.disabled = false;
+      }, 1000);
+      parsedCode.select();
+      document.execCommand("copy");
+   }
+});
+
+// Clear Button
+clearBtn.addEventListener("click", function () {
+   if (rawCode.value === "") {
+      window.alert("Oops, you haven't added any code.");
+      return false;
+   } else {
+      clearBtn.innerHTML = "Clearing...";
+      clearBtn.disabled = true;
+      setTimeout(() => {
+         rawCode.value = "";
+         parsedCode.value = "";
+         clearBtn.innerHTML = "Clear";
+         clearBtn.disabled = false;
+         parsedCodeHidden.classList.add("hidden");
+      }, 1000);
+   }
+});
